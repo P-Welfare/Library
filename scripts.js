5985f400d1666c8e2a51c846d68ec650fc0677d5
P-Welfare <pwelfare@gmail.com>
2023-09-26 23:40:48 +0700

    Updated JS with displayBook function

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        info =  `${title} by ${author}, ${pages}, ${read}.`
+        return info;
+        console.log(Book.info());
+    }
+    
+}
+
+
+function addBookToLibrary(Object) {
+    myLibrary.push(Object);
+    
+  };
+
+const bookTest = new Book('Harry', 'JK Rowling', 200, 'yes');
+const bookTest2 = new Book('Harry 2', 'JK Rowling', 201, 'yes');
+
+function displayBooks() {
+    for (let i= 0; i < myLibrary.length ; i++) {
+        const container = document.querySelector('#container'); 
+        const div = document.createElement('div');
+        div.textContent = `${myLibrary[i].title} ` + `${myLibrary[i].author}` + ` ${myLibrary[i].pages}`;
+        container.appendChild(div);
+    }
+}
