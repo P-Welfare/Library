027c14299a43d06a133c5e0d613f82f6ce2a2921
P-Welfare <pwelfare@gmail.com>
2023-09-30 01:23:32 +0700

    Added dialog box and live feed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+const showButton = document.getElementById("showDialog");
+const dialog = document.getElementById("favDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const confirmBtn = document.querySelector("#confirmBtn");
+var title = document.getElementById("title");
+var author = document.getElementById("author");
+var pages = document.getElementById("pages");
+var read = document.getElementById("read");
+
+
+
 const myLibrary = [];
 
 
@@ -24,15 +36,45 @@ const bookTest = new Book('Harry', 'JK Rowling', 200, 'yes');
 const bookTest2 = new Book('Harry 2', 'JK Rowling', 201, 'yes');
 
 function displayBooks() {
-    for (let i= 0; i < myLibrary.length ; i++) {
+     
+
+
+    for (let i= 0; i < myLibrary.length ; i++) { 
+     
+
         const container = document.querySelector('#container'); 
         const div = document.createElement('div');
         div.textContent = `${myLibrary[i].title} ` + `${myLibrary[i].author}` + ` ${myLibrary[i].pages}`;
         container.appendChild(div);
     }
-}
+    };
 
-const btn = document.getElementById('#newBook');
-btn.addEventListener('click', () => {
+
+function clearDisplay() {
+    const container = document.querySelector('#container');
+    while (container.hasChildNodes())
+    container.removeChild(container.firstChild)
+    }
+ 
+
+showButton.addEventListener("click", () => {
+    favDialog.showModal();
+  });
+
+  confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+   
+   /* title = title.value;
+    author = author.value; NOT SURE ON THIS
+    pages = pages.value;
+    read = read.value; */
     
-});
+    favDialog.close(); // Have to send the select box value here.
+    // must work out this close value an
+    var addBook = new Book(title.value,author.value,pages.value,read.value) 
+    addBookToLibrary(addBook);
+    clearDisplay();
+    displayBooks();
+    
+  });
+
